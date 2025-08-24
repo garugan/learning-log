936d6b8b320b0cda4ed100a36449df5dd38782ed
garugan <haruichi0034@gmail.com>
2025-08-25 00:41:24 +0900

    console.log～文字列の連結

--- javascript/fetch-api-sample.js
+++ javascript/fetch-api-sample.js
@@ -1,4 +1,36 @@
-// Fetch APIサンプル
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(res => res.json())
-  .then(data => console.log(data));
+// 文字の表示
+console.log("「ES6 学習コース」へようこそ！");
+console.log("にんじゃわんこと一緒にES6を学んでいきましょう。");
+
+
+// 計算
+// 5と3を足した値を出力してください
+console.log(5 + 3);
+
+// 20から8を引いた値を出力してください
+console.log(20 - 8);
+
+// "4 + 5" を文字列として出力してください
+console.log("4 + 5");
+
+
+//掛け算「*」、割り算「/」
+// 8と4をかけた結果を出力してください
+console.log(8 * 4);
+
+// 24を4で割った結果を出力してください
+console.log(24 / 4);
+
+// 7を2で割った余りを出力してください
+console.log(7 % 2);
+
+
+// 文字列の連結
+// 「ひつじ」と「仙人」を連結してコンソールに出力してください
+console.log("ひつじ" + "仙人");
+
+// 文字列の「20」と「15」を連結してコンソールに出力してください
+console.log("20" + "15");
+
+
+//
