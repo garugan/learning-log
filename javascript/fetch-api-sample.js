b20f9f9dc63b8180b58de19ed9b4f6a436b2e2bf
garugan <haruichi0034@gmail.com>
2025-08-28 17:49:21 +0900

    定数～真偽値と比較演算子

--- javascript/fetch-api-sample.js
+++ javascript/fetch-api-sample.js
@@ -86,3 +86,70 @@ console.log(number);
 
 
 
+// 定数
+// 定数languageを定義してください
+const language = "フランス語";
+
+// 定数languageの値を出力してください
+console.log(language);
+
+// 定数languageを用いて、「〇〇を話せます」と出力してください
+console.log(language + "を話せます");
+
+
+
+// テンプレートリテラル
+const name = "にんじゃわんこ";
+const age = 14;
+
+// 「ぼくの名前は〇〇です」とコンソールに出力してください
+console.log(`ぼくの名前は${name}です`);
+
+// 「今は〇〇歳です」と出力してください
+console.log(`今は${age}歳です`);
+
+
+
+// if文、条件分岐
+const level = 12;
+
+// 条件式を「level > 10」とするif文を作ってください
+if (level > 10) {
+  console.log("レベルが10より大きいです");
+}
+
+
+
+// 真偽値と比較演算子
+const age = 24;
+
+// 「age >= 20」を出力してください
+console.log(age >= 20);
+
+// 「age < 20」を出力してください
+console.log(age < 20);
+
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+}
+
+
+
+// 等価演算子
+const password = "ninjawanko";
+
+// passwordの値が"ninjawanko"の場合、「ログインに成功しました」と出力してください
+if (password == "ninjawanko"){
+  console.log("ログインに成功しました");
+}
+
+
+// passwordの値が"ninjawanko"でない場合、「パスワードが間違っています」と出力してください
+if (password != "ninjawanko"){
+  console.log("パスワードが間違っています");
+}
+
+
+
+// 
