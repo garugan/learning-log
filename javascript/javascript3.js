c7d7a1bb28c086242cf1de4e81ca735ec40d01b5
garugan <haruichi0034@gmail.com>
2025-09-11 15:57:21 +0900

    関数とは～引数とは

---
+++ javascript/javascript3.js
@@ -0,0 +1,44 @@
+// 関数とは（1）
+const greet = function() {
+  console.log("こんにちは！");
+  console.log("関数を学習していきましょう！");
+};
+
+// 関数を呼び出してください
+
+greet();
+
+
+
+// 関数とは（2）
+// 定数helloに関数を代入してください
+const hello = function() {
+  console.log("こんにちは！");
+  console.log("私の名前はぼくです");
+} ;
+
+
+// 定数helloに代入された関数を呼び出してください
+hello();
+
+
+
+// アロー関数
+// 定数greetにアロー関数を代入してください
+const greet = () => {
+  console.log("こんにちは！");
+}
+
+// 定数greetを呼び出してください
+greet ();
+
+
+
+// 引数とは
+// 定数greetにアロー関数を代入してください
+const greet = () => {
+  console.log("こんにちは！");
+}
+
+// 定数greetを呼び出してください
+greet ();
