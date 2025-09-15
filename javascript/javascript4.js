7f3db625014930b0ffc76c663f17a9df4ba2b50e
garugan <haruichi0034@gmail.com>
2025-09-15 22:43:15 +0900

    オブジェクトの復習～メソッド内でのメソッド呼び出し

---
+++ javascript/javascript4.js
@@ -0,0 +1,170 @@
+// オブジェクトの復習
+// 定数animalを定義してください
+const animal = {
+  name:"レオ",
+  age:3,
+  greet: () => {
+    console.log("こんにちは");
+  }
+};
+
+// animalのnameプロパティの値を出力してください
+console.log(animal.name);
+
+// animalのgreetプロパティの関数を実行してください
+animal.greet();
+
+
+
+// クラスとは
+// Animalクラスを定義してください
+class Animal {
+  
+}
+
+
+// インスタンスの生成
+class Animal {
+}
+
+// Animalクラスのインスタンスを定数animalに代入してください
+const animal = new Animal();
+
+// 定数animalの値を出力してください
+console.log(animal);
+
+
+
+// コンストラクタ（1）
+class Animal {
+  // コンストラクタを追加してください
+  constructor(){
+    console.log("インスタンスを生成しました");
+  }
+  
+}
+
+const animal = new Animal();
+
+
+
+// コンストラクタ（2）
+class Animal {
+  constructor() {
+    // nameの値に文字列「レオ」を代入してください
+    this.name = "レオ";
+    
+    // ageの値に数値の「3」を代入してください
+    this.age = 3;
+    }
+}
+
+const animal = new Animal();
+
+// 「名前: 〇〇」となるように出力してください
+console.log(`名前：${animal.name}`);
+
+// 「年齢: 〇〇」となるように出力してください
+console.log(`年齢：${animal.age}`);
+
+
+
+// コンストラクタ（3）
+class Animal {
+  // 引数に「name」と「age」を追加してください
+  constructor(name,age) {
+    // 「"レオ"」の代わりに引数nameの値を代入してください
+    this.name = name;
+    
+    // 「3」の代わりに引数ageの値を代入してください
+    this.age = age;
+  }
+}
+
+// 引数に「"モカ"」と「8」を渡してください
+const animal = new Animal("モカ",8);
+
+console.log(`名前: ${animal.name}`);
+console.log(`年齢: ${animal.age}`);
+
+
+
+// メソッド（1）
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  // greetメソッドを追加してください
+  greet(){
+    console.log("こんにちは");
+  }
+  
+}
+
+const animal = new Animal("レオ", 3);
+
+console.log(`名前: ${animal.name}`);
+console.log(`年齢: ${animal.age}`);
+
+// animalに対してgreetメソッドを呼び出してください
+animal.greet();
+
+
+
+// メソッド（2）
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  // infoメソッドを追加してください
+  info() {
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+  
+}
+
+const animal = new Animal("レオ", 3);
+animal.greet();
+
+// animalに対してinfoメソッドを呼び出してください
+animal.info();
+
+
+
+// メソッド内でのメソッド呼び出し
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    // greetメソッドを呼び出してください
+    this.greet();
+    
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+const animal = new Animal("レオ", 3);
+// 以下の1行を消してください
+
+animal.info();
+
+
+
+//
