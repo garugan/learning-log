2074aa926e0571def92741b45a1ae89df382a972
garugan <haruichi0034@gmail.com>
2025-09-16 20:28:29 +0900

    継承とは～オーバーライド

--- javascript/javascript4.js
+++ javascript/javascript4.js
@@ -167,4 +167,173 @@ animal.info();
 
 
 
-//
+// 継承とは
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+// Animalクラスを継承してDogクラスを定義してください
+class Dog extends Animal {
+  
+}
+
+
+const animal = new Animal("レオ", 3);
+animal.info();
+
+
+
+// 継承したクラスを使う
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+}
+
+// 定数dogにDogクラスのインスタンスを代入してください
+const dog = new Dog("レオ",4);
+
+// dogに対してinfoメソッドを呼び出してください
+dog.info();
+
+
+
+// メソッドの追加
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // getHumanAgeメソッドを追加してください
+  getHumanAge() {
+    return this.age *7;
+  }
+
+}
+
+const dog = new Dog("レオ", 4);
+dog.info();
+
+// 定数humanAgeを定義し、定数dogに対してgetHumanAgeメソッドを呼び出した値を代入してください
+const humanAge = dog.getHumanAge();
+
+// 「人間年齢で〇〇歳です」と出力してください
+console.log(`人間年齢で${humanAge}歳です`);
+
+
+
+// オーバーライド（1）
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // infoメソッドを追加してください
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+    
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
+
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("レオ", 4);
+dog.info();
+
+
+
+// オーバーライド（2）
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // infoメソッドを追加してください
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+    
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
+
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("レオ", 4);
+dog.info();
